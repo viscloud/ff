51e0c80a3afd0e0412962790f1045a9050b7c873
Christopher Canel <c.canel@icloud.com>
2019-04-22 14:22:28 -0400

    Update script to batch-fix ff_conf files

--- scripts/e2e/configs/ff_confs/edit_ff_confs.sh
+++ scripts/e2e/configs/ff_confs/edit_ff_confs.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Copyright 2016 The FilterForward Authors. All Rights Reserved.
@@ -15,55 +16,68 @@
 # limitations under the License.
 
 
-       # ./window5_pedestrian_uc/config-60.csv \
-       # ./window5_pedestrian_uc/config-20.csv \
-       # ./window5_pedestrian_uc/config-5.csv \
-       # ./window5_pedestrian_uc/config-10.csv \
-       # ./window5_pedestrian_uc/config-50.csv \
-       # ./window5_pedestrian_uc/config-40.csv \
-       # ./window5_pedestrian_uc/config-30.csv \
-       # ./window5_pedestrian_uc/config-1.csv \
-# FILES=(./person_classifier_1x1_objdet/config-60.csv \
-#        ./person_classifier_1x1_objdet/config-35.csv \
-#        ./person_classifier_1x1_objdet/config-20.csv \
-#        ./person_classifier_1x1_objdet/config-25.csv \
-#        ./person_classifier_1x1_objdet/config-5.csv \
-#        ./person_classifier_1x1_objdet/config-10.csv \
-#        ./person_classifier_1x1_objdet/config-50.csv \
-#        ./person_classifier_1x1_objdet/config-40.csv \
-#        ./person_classifier_1x1_objdet/config-15.csv \
-#        ./person_classifier_1x1_objdet/config-55.csv \
-#        ./person_classifier_1x1_objdet/config-30.csv \
-#        ./person_classifier_1x1_objdet/config-45.csv \
-#        ./person_classifier_1x1_objdet/config-1.csv)
-FILES=(./windowed1/config-60.csv \
-       ./windowed1/config-35.csv \
-       ./windowed1/config-20.csv \
-       ./windowed1/config-25.csv \
-       ./windowed1/config-5.csv \
-       ./windowed1/config-10.csv \
-       ./windowed1/config-50.csv \
-       ./windowed1/config-40.csv \
-       ./windowed1/config-15.csv \
-       ./windowed1/config-55.csv \
-       ./windowed1/config-30.csv \
-       ./windowed1/config-45.csv \
-       ./windowed1/config-1.csv)
-# FILES=(./person_gr8_classifier/config-60.csv \
-#        ./person_gr8_classifier/config-35.csv \
-#        ./person_gr8_classifier/config-20.csv \
-#        ./person_gr8_classifier/config-25.csv \
-#        ./person_gr8_classifier/config-5.csv \
-#        ./person_gr8_classifier/config-10.csv \
-#        ./person_gr8_classifier/config-50.csv \
-#        ./person_gr8_classifier/config-40.csv \
-#        ./person_gr8_classifier/config-15.csv \
-#        ./person_gr8_classifier/config-55.csv \
-#        ./person_gr8_classifier/config-30.csv \
-#        ./person_gr8_classifier/config-45.csv \
-#        ./person_gr8_classifier/config-1.csv)
+FILES=(./1x1_objdet/config-1.csv \
+       ./1x1_objdet/config-10.csv \
+       ./1x1_objdet/config-15.csv \
+       ./1x1_objdet/config-2.csv \
+       ./1x1_objdet/config-20.csv \
+       ./1x1_objdet/config-25.csv \
+       ./1x1_objdet/config-3.csv \
+       ./1x1_objdet/config-30.csv \
+       ./1x1_objdet/config-35.csv \
+       ./1x1_objdet/config-4.csv \
+       ./1x1_objdet/config-40.csv \
+       ./1x1_objdet/config-45.csv \
+       ./1x1_objdet/config-5.csv \
+       ./1x1_objdet/config-50.csv \
+       ./1x1_objdet/config-55.csv \
+       ./1x1_objdet/config-6.csv \
+       ./1x1_objdet/config-60.csv \
+       ./1x1_objdet/config-7.csv \
+       ./1x1_objdet/config-8.csv \
+       ./1x1_objdet/config-9.csv \
+       ./spatial_crop/config-1.csv \
+       ./spatial_crop/config-10.csv \
+       ./spatial_crop/config-15.csv \
+       ./spatial_crop/config-2.csv \
+       ./spatial_crop/config-20.csv \
+       ./spatial_crop/config-25.csv \
+       ./spatial_crop/config-3.csv \
+       ./spatial_crop/config-30.csv \
+       ./spatial_crop/config-35.csv \
+       ./spatial_crop/config-4.csv \
+       ./spatial_crop/config-40.csv \
+       ./spatial_crop/config-45.csv \
+       ./spatial_crop/config-5.csv \
+       ./spatial_crop/config-50.csv \
+       ./spatial_crop/config-55.csv \
+       ./spatial_crop/config-6.csv \
+       ./spatial_crop/config-60.csv \
+       ./spatial_crop/config-7.csv \
+       ./spatial_crop/config-8.csv \
+       ./spatial_crop/config-9.csv \
+       ./windowed/config-1.csv \
+       ./windowed/config-10.csv \
+       ./windowed/config-15.csv \
+       ./windowed/config-2.csv \
+       ./windowed/config-20.csv \
+       ./windowed/config-25.csv \
+       ./windowed/config-3.csv \
+       ./windowed/config-30.csv \
+       ./windowed/config-35.csv \
+       ./windowed/config-4.csv \
+       ./windowed/config-40.csv \
+       ./windowed/config-45.csv \
+       ./windowed/config-5.csv \
+       ./windowed/config-50.csv \
+       ./windowed/config-55.csv \
+       ./windowed/config-6.csv \
+       ./windowed/config-60.csv \
+       ./windowed/config-7.csv \
+       ./windowed/config-8.csv \
+       ./windowed/config-9.csv)
 
 for FILE in ${FILES[@]}; do
     echo "Editing: $FILE"
-    sed -i 's/conv2d_1_input/inputs/g' $FILE
+    sed -i 's+/home/ccanel/src/filterforward-doc/papers/ff+~/src/filterforward+g' $FILE
 done
